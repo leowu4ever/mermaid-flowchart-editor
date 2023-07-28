12c5858fb94adc29be4f8a59254f35234dbfd0c1
Leo Wu <leowu4ever@users.noreply.github.com>
2023-07-28 22:00:56 +0000

    - updated layout

--- app.py
+++ app.py
@@ -1,57 +1,60 @@
 import streamlit as st
 from streamlit_mermaid import st_mermaid
+   
+def add_new_node(ref, name):
+    global code
+    code += f'\n{ref}[{name}]'
+        
+if __name__ == '__main__':
+    code = '''
+           flowchart
+           '''
+    st.set_page_config(layout='wide')
+    st.title('Mermaid visual editor')
 
-def add_new_node(code, id, name):
-    print(code)
-    code = f'{code}\n{id}[{name}]'
-    print(code)
+    col_config, col_display, col_code = st.columns([3,4,2])
+    with col_config:
+        st.markdown('''
+                    This tool aims to help you create some common diagrams supported by Mermaid.
+                    Currently it can create flow chart.
+                    ''')
+        st.selectbox(label='Please select a diagram type', options=['Flow chart', 'Class chart', 'Gantt chart'])
+        col_node_ref, col_node_name = st.columns(2)
+        with col_node_ref:
+            ref = st.text_input('Node reference')
+        with col_node_name:
+            name = st.text_input('Node name')
+        
+       
+        col_add_node, col_remove_node, col_edit_node = st.columns(3)
+        with col_add_node:
+            btn_add_node = st.button('Add a node',  use_container_width=True)
+            if btn_add_node:
+                code += f'\n{ref}[{name}]'
+                
+        with col_remove_node:
+            st.button('Remove a node', use_container_width=True)
+        with col_edit_node:
+            st.button('Edit a node',  use_container_width=True)
 
-st.set_page_config(layout='wide')
-code = '''
-erDiagram
-    CUSTOMER }|..|{ DELIVERY-ADDRESS : has
-    CUSTOMER ||--o{ ORDER : places
-    CUSTOMER ||--o{ INVOICE : "liable for"
-    DELIVERY-ADDRESS ||--o{ ORDER : receives
-    INVOICE ||--|{ ORDER : covers
-    ORDER ||--|{ ORDER-ITEM : includes
-    PRODUCT-CATEGORY ||--|{ PRODUCT : contains
-    PRODUCT ||--o{ ORDER-ITEM : "ordered in"
-    '''
-st.title('Mermaid visual editor')
+        col_node_a, col_node_b = st.columns(2)
+        with col_node_a:
+            st.text_input('Node id (A)')
+        with col_node_b:
+            st.text_input('Node id (B)')    
 
-col_config, col_display, col_code = st.columns([2,4,2])
-with col_config:
-    st.markdown('''
-                This tool aims to help you create some common diagram supported by Mermaid.
-                Currently it can create flowchart.
-                ''')
-    st.selectbox(label='Please select a diagram type', options=['Flow chart', 'Class chart', 'Gantt chart'])
-    col_node_id, col_node_name = st.columns(2)
-    with col_node_id:
-        id = st.text_input('Node id')
-    with col_node_name:
-        name = st.text_input('Node name')
-         
-    col_add_node, col_remove_node, col_edit_node = st.columns(3)
-
-    with col_add_node:
-        st.button('Add',  use_container_width=True, on_click=add_new_node, kwargs={'code': code, 'id': id, 'name': name})
-    with col_remove_node:
-        st.button('Remove', use_container_width=True)
-    with col_edit_node:
-        st.button('Edit',  use_container_width=True)
-
-    connect_node_a, connect_node_b = st.columns(2)
-    with connect_node_a:
-        st.text_input('Node id (A)')
-        st.button('Connect')
-    with connect_node_b:
-        st.text_input('Node id (B)')    
-
-with col_display:
-    st_mermaid(code, height=1000)
-    
-with col_code:
-    st.subheader('Code generated')
-    st.write(code)
+        col_add_edge, col_remove_edge, col_edit_edge = st.columns(3)
+        with col_add_edge:
+            st.button('Add an edge', use_container_width=True)
+        with col_remove_edge:
+            st.button('Remove an edge', use_container_width=True)
+        with col_edit_edge:
+            st.button('Edit an edge', use_container_width=True)
+        
+    with col_display:
+        st.subheader('Chart generated')
+        st_mermaid(code, height=1000)
+        
+    with col_code:
+        st.subheader('Code generated')
+        code_block = st.code(code, language='mermaid', line_numbers=True)
