a2bdabf5254174c03a4dc92581ffa5c786c77c58
Leo Wu <leowu4ever@users.noreply.github.com>
2023-07-28 20:39:38 +0000

    -temp save

--- app.py
+++ app.py
@@ -6,27 +6,33 @@ def add_new_node(code, id, name):
     code = f'{code}\n{id}[{name}]'
     print(code)
 
+st.set_page_config(layout='wide')
 code = '''
-    flowchart
-    a
+erDiagram
+    CUSTOMER }|..|{ DELIVERY-ADDRESS : has
+    CUSTOMER ||--o{ ORDER : places
+    CUSTOMER ||--o{ INVOICE : "liable for"
+    DELIVERY-ADDRESS ||--o{ ORDER : receives
+    INVOICE ||--|{ ORDER : covers
+    ORDER ||--|{ ORDER-ITEM : includes
+    PRODUCT-CATEGORY ||--|{ PRODUCT : contains
+    PRODUCT ||--o{ ORDER-ITEM : "ordered in"
     '''
-st.markdown('''
-            # Mermaid visual editor
-            ''')
+st.title('Mermaid visual editor')
 
-col_config, col_display = st.columns(2)
+col_config, col_display, col_code = st.columns([2,4,2])
 with col_config:
     st.markdown('''
-            This tool aims to help you create some common diagram supported by Mermaid.
-            Currently it can create flowchart.
-            ''')
-    st.selectbox(label='Please select a diagram type', options=['Flowchart'])
+                This tool aims to help you create some common diagram supported by Mermaid.
+                Currently it can create flowchart.
+                ''')
+    st.selectbox(label='Please select a diagram type', options=['Flow chart', 'Class chart', 'Gantt chart'])
     col_node_id, col_node_name = st.columns(2)
     with col_node_id:
         id = st.text_input('Node id')
     with col_node_name:
         name = st.text_input('Node name')
-        
+         
     col_add_node, col_remove_node, col_edit_node = st.columns(3)
 
     with col_add_node:
@@ -41,8 +47,11 @@ with col_config:
         st.text_input('Node id (A)')
         st.button('Connect')
     with connect_node_b:
-        st.text_input('Node id (B)')
-        
+        st.text_input('Node id (B)')    
 
 with col_display:
-    st_mermaid(code, height="500px")
+    st_mermaid(code, height=1000)
+    
+with col_code:
+    st.subheader('Code generated')
+    st.write(code)
