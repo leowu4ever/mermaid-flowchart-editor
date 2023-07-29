2f31b0ef8c9e88d7709d1ce80581d34c659a75e3
Leo Wu <leowu4ever@users.noreply.github.com>
2023-07-29 09:11:45 +0000

    - updated layout

--- app.py
+++ app.py
@@ -1,60 +1,48 @@
 import streamlit as st
 from streamlit_mermaid import st_mermaid
-   
-def add_new_node(ref, name):
-    global code
-    code += f'\n{ref}[{name}]'
+
+def add_node():
+    st.session_state['nodes'][st.session_state['node_title']] = st.session_state['node_title']
+    st.session_state['node_title'] = ''
         
 if __name__ == '__main__':
-    code = '''
-           flowchart
-           '''
+    # Initialization
+    if 'code' not in st.session_state:
+        st.session_state['code'] = 'flowchart'
+    
+    if 'nodes' not in st.session_state:
+        st.session_state['nodes'] = {}
+    
+    if 'edge' not in st.session_state:
+        st.session_state['edge'] = {}
+            
+    if 'subgraphs' not in st.session_state:
+        st.session_state['subgraphs'] = {}
+        
     st.set_page_config(layout='wide')
     st.title('Mermaid visual editor')
 
-    col_config, col_display, col_code = st.columns([3,4,2])
+    col_config, col_display = st.columns([3.5,6])
     with col_config:
         st.markdown('''
                     This tool aims to help you create some common diagrams supported by Mermaid.
                     Currently it can create flow chart.
                     ''')
         st.selectbox(label='Please select a diagram type', options=['Flow chart', 'Class chart', 'Gantt chart'])
-        col_node_ref, col_node_name = st.columns(2)
-        with col_node_ref:
-            ref = st.text_input('Node reference')
-        with col_node_name:
-            name = st.text_input('Node name')
-        
-       
-        col_add_node, col_remove_node, col_edit_node = st.columns(3)
-        with col_add_node:
-            btn_add_node = st.button('Add a node',  use_container_width=True)
-            if btn_add_node:
-                code += f'\n{ref}[{name}]'
-                
-        with col_remove_node:
-            st.button('Remove a node', use_container_width=True)
-        with col_edit_node:
-            st.button('Edit a node',  use_container_width=True)
 
-        col_node_a, col_node_b = st.columns(2)
-        with col_node_a:
-            st.text_input('Node id (A)')
-        with col_node_b:
-            st.text_input('Node id (B)')    
+        col_node_title, col_node_shape, col_node_add = st.columns([5,2.5,2.5])
+        col_node_title.text_input('title', label_visibility='collapsed', placeholder='node title', on_change=add_node, key='node_title')
+        col_node_shape.selectbox('shape', options=['square', 'round', 'container'], label_visibility='collapsed')
+        col_node_add.button('add', on_click=add_node, use_container_width=True)
+        col_nodes_added, col_node_remove, col_node_edit = st.columns([5,1.5,1.5])
+        col_nodes_added.selectbox('Nodes added', st.session_state['nodes'].values(), label_visibility='collapsed')
+        col_node_remove.button('remove', use_container_width=True)
+        col_node_edit.button('edit', use_container_width=True)
 
-        col_add_edge, col_remove_edge, col_edit_edge = st.columns(3)
-        with col_add_edge:
-            st.button('Add an edge', use_container_width=True)
-        with col_remove_edge:
-            st.button('Remove an edge', use_container_width=True)
-        with col_edit_edge:
-            st.button('Edit an edge', use_container_width=True)
-        
+        st.code(st.session_state['code'], language='mermaid', line_numbers=True)
+
     with col_display:
-        st.subheader('Chart generated')
-        st_mermaid(code, height=1000)
+        st_mermaid(st.session_state['code'], height=1000)
         
     with col_code:
         st.subheader('Code generated')
-        code_block = st.code(code, language='mermaid', line_numbers=True)
