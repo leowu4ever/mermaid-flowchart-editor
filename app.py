d3223fbfd653d66f1bbd8e3fb42062a573640978
Leo <wupopi@gmail.com>
2023-07-29 22:45:58 +0100

    - updated layout

--- app.py
+++ app.py
@@ -64,6 +64,7 @@ def update_code():
     
             
 if __name__ == '__main__':
+    # init session state
     if 'code' not in st.session_state:
         st.session_state['code'] = ''
     if 'nodes' not in st.session_state:
@@ -81,38 +82,41 @@ if __name__ == '__main__':
         
     col_config, col_display, col_code = st.columns([2.5,3,2])
     with col_config:
+        # config
         st.subheader('Config')
         col_direction, col_theme = st.columns(2)
         col_direction.selectbox('Chart direction', ['From left to right', 'from top to bottom'])
         col_theme.selectbox('Chart theme', ['base', 'forest', 'dark'])
-        st.subheader('Node')
-        col_node_title, col_node_shape, col_node_add = st.columns([3,1.5, 1.5])
-        col_node_title.text_input(label='', placeholder='node title', label_visibility='collapsed', on_change=add_node, key='node_title')
-        col_node_shape.selectbox('Shape', label_visibility='collapsed', options=['square', 'elipse', 'container'])
-        col_node_add.button('add', on_click=add_node, use_container_width=True)
         
-        col_node_selected, col_node_remove = st.columns([4.5, 1.47])
-        col_node_selected.selectbox('All nodes added', list(st.session_state['nodes'].values())[::-1], label_visibility='collapsed', key='node_remove_selected')
-        col_node_remove.button('remove', on_click=remove_node, use_container_width=True)
+        # node - add
+        st.subheader('Node')
+        col_node_title, col_node_shape = st.columns([3,1])
+        col_node_title.text_input(label='Title', on_change=add_node, key='node_title')
+        col_node_shape.selectbox('Shape', options=['square', 'ellipse', 'container'])
+        st.button('add a new node', on_click=add_node, use_container_width=True)
+        # node - remove
+        st.selectbox('Select a node to remove', list(st.session_state['nodes'].values())[::-1], key='node_remove_selected')
+        st.button('remove an existing node', on_click=remove_node, use_container_width=True)
 
+        # edge - add
         st.subheader('Edge')
         col_node_a, col_node_b = st.columns(2)
-        col_node_a.selectbox('Node A', options=st.session_state['nodes'], key='node_a')
-        col_node_b.selectbox('Node B', options=st.session_state['nodes'], key='node_b')
+        col_node_a.selectbox('Node/group A', options=st.session_state['nodes'], key='node_a')
+        col_node_b.selectbox('Node/group B', options=st.session_state['nodes'], key='node_b')
+        st.button('add an edge', use_container_width=True, on_click=add_edge)
+        # edge - remove
+        st.selectbox('Select an edge to remove', st.session_state['edges'].items())
+        st.button('remove an existing edge', use_container_width=True, on_click=remove_edge)
         
-        col_edge_add, col_edge_remove = st.columns(2)
-        col_edge_add.button('add an edge', use_container_width=True, on_click=add_edge)
-        col_edge_remove.button('remove an edge', use_container_width=True, on_click=remove_edge)
-        
+        # group - add
         st.subheader('Group')
-        st.multiselect('All nodes added', options=st.session_state['nodes'], label_visibility='collapsed', key='group_nodes')
+        st.multiselect('Select one/multiple node(s) to group', options=st.session_state['nodes'], key='group_nodes')
         col_group_name, col_group_create = st.columns([1,1])
         col_group_name.text_input('group name', label_visibility='collapsed', placeholder='group name', key='group_name')
         col_group_create.button('group', use_container_width=True, on_click=group_nodes)
-        
-        col_group_selected, col_group_ungroup = st.columns([1,1])
-        col_group_selected.selectbox('', st.session_state['groups'].keys(), label_visibility='collapsed', key='group_selected')
-        col_group_ungroup.button('ungroup', use_container_width=True, on_click=ungroup_nodes)
+        # group - remove
+        st.selectbox('Select a group to ungroup', st.session_state['groups'].keys(), key='group_selected')
+        st.button('ungroup an existing group', use_container_width=True, on_click=ungroup_nodes)
         
     with col_display:
         if st.session_state['code'] != '':
